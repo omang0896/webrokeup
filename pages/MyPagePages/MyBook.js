436b3a8dfc8bbdd8cf846ce3dbe9f736be5411bd
omang0896 <57280296+omang0896@users.noreply.github.com>
2021-06-26 17:05:59 +0900

    Update MyBook.js

--- pages/MyPagePages/MyBook.js
+++ pages/MyPagePages/MyBook.js
@@ -1,20 +1,14 @@
 import React, {useEffect, useState} from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import {firebase_db} from '../../firebaseConfig'
 
 
-const ChapterItem = (props) => {
-    const {navigation, chapters, chapterTitle}=props;
-    
-    
-    const isMyitem = (typeof myitem !== 'undefined');
-    if (isMyitem == false) {
-        return null; // early return
-    }
+const MyChapterItem = (props) => {
+    const {navigation, chapters, chapterTitle, myitem} = props;
+
     return (
         <View>
-            <TouchableOpacity style={styles.bookIndexOne} onPress={()=>{navigation.navigate('readArticle', {chapters: chapters, chapterTitle: chapters.chapterTitle})}}>
+            <TouchableOpacity style={styles.bookIndexOne} onPress={()=>{navigation.navigate('MyArticle', {myitem: myitem, chapters:chapters, navigation:navigation, chapterTitle:chapterTitle})}}>
                 <Text style={styles.bookIndexOneTitle}>{chapters.chapterTitle}</Text>  
                 <Text style={styles.bookIndexOnePunchLine}>{chapters.mainText}</Text>
                 <Text style={styles.bookIndexText}>{chapters.regdate}</Text>
@@ -24,80 +18,82 @@ const ChapterItem = (props) => {
     )
 }
 
-
-const MyBook = ({navigation, route}) => {
+const MyBook = ({navigation, route}) =>{
     const {myitem} = route.params;
+    const intro = Object.values(myitem.intro)
+    const chapters = Object.values(myitem.chapters)
 
-    function renderMyChapter() {
-        const isMyitem = (typeof myitem !== 'undefined')
-        if (isMyitem == false){
-            return null;
-        }
+    console.log(intro)
+
+    const renderMyChapter = () => {
+        console.log("renderMyChapter실행중")
+
+        const isMyitem = (typeof myitem !== 'undefined');
+            if (isMyitem == false) {
+                return null; 
+            }
+    
+        const isChapters = (myitem.chapters > ''); 
+            if (isChapters == false) {
+                return null; 
+            }
+            // console.log(myitem.chapters)
+
+        const subChapters = Object.values(chapters)
+        console.log(subChapters)
+        
+        return(
+            subChapters.map(chapters=>{
+                return(
+                    <MyChapterItem
+                        navigation = {navigation}
+                        chapters = {chapters}
+                        chapterTitle = {chapters.chapterTitle}
+                        myitem = {myitem}
+                    />
+                )
+            })
+        )
     }
-
-        const isMyChapters = (myitem.chapters > '');
-        if (isMyChapters == false) {
-            return null;
-        }
-
-        const {mychapters} = myitem.chapters;
-        const list = Object.keys(myitem);
-        const ch_list = Object.keys(list);
-
-        const chlist = ch_list.map(key => {
-            const isMyChaptersItem = chapters[key]
-
-
-            return (
-            <ChapterItem 
-                navigation={navigation}
-                chapters={chapterItemChapters}
-                chapterTitle={chapters.chapterTitle}
-            />);
-        })
-
-       
-
+    
 
     return (
-    <ScrollView style={styles.container}>
-        <View  style={styles.bookCoverContainer}>
-            <StatusBar style="white" />
-            <Image style={styles.bookCoverImage} source={{uri:myitem.image}}></Image>
-        </View>
-        <View  style={styles.bookInfoContainer}>
-            <Text style={styles.bookTitle}>{myitem.bookTitle}</Text>
-            <Text style= {{padding:15}}>{myitem.regdate}</Text>
-            <Text style={styles.bookDesc}>{myitem.intro.introArticle}</Text>  
-            <TouchableOpacity style={styles.subButton}>        
-                <Text style={styles.subButtonText}>구독하기</Text>
-            </TouchableOpacity>   
-        </View>
-        <View style={styles.bookIndexContainer}>
-            { renderMyChapter() }
-        </View>
-    </ScrollView>
-    );
-}
 
-const styles = StyleSheet.create({ 
+            <ScrollView style={styles.container}>
+                <View  style={styles.bookCoverContainer}>
+                    <StatusBar style="white" />
+                    <Image style={styles.bookCoverImage} source={{uri:myitem.image}}></Image>
+                </View>
+                <View  style={styles.bookInfoContainer}>
+                    <Text style={styles.bookTitle}>{myitem.bookTitle}</Text>  
+                    <Text style={styles.bookDesc}>{intro}</Text>  
+                    <TouchableOpacity style={styles.subButton}>        
+                        <Text style={styles.subButtonText}>구독하기</Text>
+                    </TouchableOpacity>   
+                </View>
+                <View style={styles.bookIndexContainer}>
+                    {renderMyChapter()}
+                </View>
+                <TouchableOpacity style = {{alignSelf: "center", padding: 20, paddingHorizontal: 150,backgroundColor: "#fe8d6f"}}
+                    onPress = {()=>navigation.navigate("NewPage", {myitem:myitem, chapters:chapters})}>
+                    <Text style = {{fontWeight:"600"}}>새로운 챕터 만들기</Text>
+                </TouchableOpacity>
+                
+            </ScrollView>
 
+        );
+    }
+    
+    
+const styles = StyleSheet.create({
     container: {
-        //앱의 배경 색
         backgroundColor:"#F5F4F4",
                 flex:1
-      },
-
-
+        },    
     bookCoverContainer: {
-
         width:'90%',
-        //컨텐츠의 높이 값
         height:220,
-        //컨텐츠의 모서리 구부리기
         marginTop:"%",
-        //컨텐츠 자체가 앱에서 어떤 곳에 위치시킬지 결정(정렬기능)
-        //각 속성의 값들은 공식문서에 고대로~ 나와 있음
         alignSelf:"center"
     },
     bookCoverImage: {
@@ -105,33 +101,24 @@ const styles = StyleSheet.create({
         height:"100%",
         width: "40%",
         alignSelf:"center"
-
-
     },
-   
+    
     bookInfoContainer: {
         backgroundColor:"#F5F4F4",
         width:'90%',
-        //컨텐츠의 높이 값
         height:130,
-        //컨텐츠의 모서리 구부리기
-        //컨텐츠 자체가 앱에서 어떤 곳에 위치시킬지 결정(정렬기능)
-        //각 속성의 값들은 공식문서에 고대로~ 나와 있음
-        alignSelf:"center",
-
+        alignSelf:"center"
     },
     bookTitle: {
         fontSize: 15,
         marginTop: "7%",
         marginLeft:"5%",
-        fontSize: 15,
-        
+        fontSize: 15,  
     },
     bookDesc: {
         marginTop: "4%",
         marginLeft:"5%",
     },
-
     subButton:{
         width:"50%",
             height:27,
@@ -142,52 +129,35 @@ const styles = StyleSheet.create({
             marginLeft: "0%",
             marginTop: "5%",
             alignSelf:"center",    
-
     },
-
     subButtonText: {
         color:"white",
         fontWeight:"200",
         //텍스트의 현재 위치에서의 정렬 
         textAlign:"center"
     },
-
-
     bookIndexContainer:{
         backgroundColor: '#fff',
-
     },
     bookIndexOne: {
         marginTop:"5%",
-      marginLeft:"5%",
-      marginRight:"3%",
-      marginBottom:"5%",
+        marginLeft:"5%",
+        marginRight:"3%",
+        marginBottom:"5%"
     },
 
     bookIndexOneTitle: {
         fontSize: 15,
-
-
     },
     bookIndexOnePunchLine:{
         fontWeight: '700',
         marginLeft:"5%",
         marginTop:"2%",
-
-
     },
     bookIndexText :{
         marginLeft:"5%",
         marginTop:"2%",
-
-
     },
-  
-
-
-
-
-
 })
 
 export default MyBook;
